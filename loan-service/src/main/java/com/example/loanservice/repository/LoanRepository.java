bb08cbec845e84aabb6229d8681dfc6c6f91f77c
Carolina Atampis <carolinanatampis@gmail.com>
2022-06-26 15:27:56 -0300

    Actualización trabajo practico

--- loan-service/src/main/java/com/example/loanservice/repository/LoanRepository.java
+++ loan-service/src/main/java/com/example/loanservice/repository/LoanRepository.java
@@ -3,5 +3,7 @@ package com.example.loanservice.repository;
 import com.example.loanservice.entity.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+
 }
